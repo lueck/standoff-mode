f434f6123db567700ed395d39256d4530d0dd7e3
Christian Lück <christian.lueck@ruhr-uni-bochum.de>
2015-05-19 00:54:44 +0200

    Added headers with copyright and license information

--- standoff-api.el
+++ standoff-api.el
@@ -1,4 +1,23 @@
-;; API for backends
+;; standoff-api.el --- API for back-ends in standoff-mode
+
+;; Copyright (C) 2015 Christian Lueck
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with SPARQL mode. If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
 
 ;; See standoff-dummy.el for a reference implementation
 
