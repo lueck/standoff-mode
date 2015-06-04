1e9da33b081f235701b6092341976c91b8972501
Steve Purcell <steve@sanityinc.com>
2015-06-04 23:39:15 +1200

    Shorten package description

--- standoff-mode-pkg.el
+++ standoff-mode-pkg.el
@@ -1,3 +1,3 @@
 (define-package "standoff-mode"
   "0.2.2"
-  "Create stand-off markup, also called external markup. A major mode written for use in the field of digital humanities and the manual annotation of training data for named-entity recognition.")
+  "Create stand-off markup, also called external markup.")
