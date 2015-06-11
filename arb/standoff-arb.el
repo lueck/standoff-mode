353c7f6a1da79d32e3ca39170126da259577190c
Christian Lück <christian.lueck@ruhr-uni-bochum.de>
2015-06-12 00:55:49 +0200

    ArB config improved
    - using eval-after-load instead of mode-hook
    
    - setting restictiveness of markup type and predicates depending of
      presence of schema

--- arb/standoff-arb.el
+++ arb/standoff-arb.el
@@ -1,102 +1,115 @@
-(require 'standoff-mode)
+;;; standoff-arb.el --- config file for Archiv des Beispiels research project.
+
+;;; Code
+
+;;(require 'standoff-mode)
 
 (add-to-list 'auto-mode-alist '("\\.TEI-P5.xml$" . standoff-mode))
 
-(defface standoff-arb-beispiel
-  '((((type x))
-     (:family
-      "misc-fixed"
-      :foreground
-      "dark red"
-      :weight
-      normal
+(eval-after-load "standoff-mode"
+  '(progn
+
+     (message "evaluating configuration for Archiv des Beispiels...")
+
+    ;; Sprache des Benutzerhandbuchs
+    (setq standoff-info-language "de")
+
+    ;; Label statt fqd-Namen für Markup-Elemente
+    (setq standoff-show-labels t)
+
+    ;; load annotation schema, no error if file not present
+    (load "standoff-arb-schema.el" t)
+
+    ;; set how restrictive markup types and predicates are handled
+    ;; depending on presence of annotation schema
+    (if (featurep 'standoff-arb-schema)
+	(setq standoff-markup-type-require-match t
+	      standoff-predicate-require-match t)
+      (setq standoff-markup-type-require-match 'confirm
+	    standoff-predicate-require-match 'confirm))
+
+    (defface standoff-arb-beispiel
+      '((((type x))
+	 (:family
+	  "misc-fixed"
+	  :foreground
+	  "dark red"
+	  :weight
+	  normal
+	  :slant
+	  normal))
+	(t
+	 (:foreground
+	  "dark red"
+	  :weight
+	  normal
+	  :slant
+	  normal)))
+      "Face used \"beispiel\" markup type."
+      :group 'standoff-faces)
+
+    (defface standoff-arb-konzept
+      '((((type x))
+	 (:family
+	  "misc-fixed"
+	  :foreground
+	  "forest green"
+	  :weight
+	  normal
       :slant
       normal))
-    (t
-     (:foreground
-      "dark red"
-      :weight
-      normal
-      :slant
-      normal)))
-  "Face used \"beispiel\" markup type."
-  :group 'standoff-faces)
+	(t
+	 (:foreground
+	  "forest green"
+	  :weight
+	  normal
+	  :slant
+	  normal)))
+      "Face used \"konzept\" markup type."
+      :group 'standoff-faces)
 
-(defface standoff-arb-konzept
-  '((((type x))
-     (:family
-      "misc-fixed"
-      :foreground
-      "forest green"
-      :weight
-      normal
-      :slant
-      normal))
-    (t
-     (:foreground
-      "forest green"
-      :weight
-      normal
-      :slant
-      normal)))
-  "Face used \"konzept\" markup type."
-  :group 'standoff-faces)
+    (defface standoff-arb-marker
+      '((((type x))
+	 (:family
+	  "misc-fixed"
+	  :foreground
+	  "dark orange"
+	  :weight
+	  normal
+	  :slant
+	  normal))
+	(t
+	 (:foreground
+	  "dark orange"
+	  :weight
+	  normal
+	  :slant
+	  normal)))
+      "Face used \"marker\" markup type."
+      :group 'standoff-faces)
 
-(defface standoff-arb-marker
-  '((((type x))
-     (:family
-      "misc-fixed"
-      :foreground
-      "dark orange"
-      :weight
-      normal
-      :slant
-      normal))
-    (t
-     (:foreground
-      "dark orange"
-      :weight
-      normal
-      :slant
-      normal)))
-  "Face used \"marker\" markup type."
-  :group 'standoff-faces)
+    (defface standoff-arb-kontext
+      '((((type x))
+	 (:family
+	  "misc-fixed"
+	  :foreground
+	  "dark blue"
+	  :weight
+	  normal
+	  :slant
+	  normal))
+	(t
+	 (:foreground
+	  "dark blue"
+	  :weight
+	  normal
+	  :slant
+	  normal)))
+      "Face used \"kontext\" markup type."
+      :group 'standoff-faces)
 
-(defface standoff-arb-kontext
-  '((((type x))
-     (:family
-      "misc-fixed"
-      :foreground
-      "dark blue"
-      :weight
-      normal
-      :slant
-      normal))
-    (t
-     (:foreground
-      "dark blue"
-      :weight
-      normal
-      :slant
-      normal)))
-  "Face used \"kontext\" markup type."
-  :group 'standoff-faces)
-
-
-
-(defun standoff-arb-config ()
-  (interactive)
-
-  (setq standoff-info-language "de")
-
-  ;; set how restrictive markup types and predicates are handled:
-  (setq standoff-markup-type-require-match t)
-  (setq standoff-predicate-require-match t);;'confirm)
-
-  (setq standoff-show-labels t)
-
-  (setq standoff-markup-overlays
-	'(("beispiel" 
+    (setq standoff-markup-overlays
+	'(("beispiel"
 	   ('priority 10)
 	   ('face 'standoff-arb-beispiel);;(:foreground "dark red"))
 	   )
@@ -114,50 +127,38 @@
 	   )
 	  ))
 
-  (setq standoff-markup-overlays-front
-	'(("beispiel" 
-	   ('face 'standoff-arb-beispiel));;(:foreground "dark red")))
-	  ("marker"
-	   ('face 'standoff-arb-marker));;(:foreground "dark blue")))
-	  ("konzept"
-	   ('face 'standoff-arb-konzept));;(:foreground "forest green")))
-	  ("kontext"
-	   ('face 'standoff-arb-kontext))
-	  ))
+    (setq standoff-markup-overlays-front
+	  '(("beispiel"
+	     ('face 'standoff-arb-beispiel));;(:foreground "dark red")))
+	    ("marker"
+	     ('face 'standoff-arb-marker));;(:foreground "dark blue")))
+	    ("konzept"
+	     ('face 'standoff-arb-konzept));;(:foreground "forest green")))
+	    ("kontext"
+	     ('face 'standoff-arb-kontext))
+	    ))
   
-  (setq standoff-markup-overlays-after
-	'(("beispiel" 
-	   ('face 'standoff-arb-beispiel));;(:foreground "dark red")))
-	  ("marker"
-	   ('face 'standoff-arb-marker));;(:foreground "dark blue")))
-	  ("konzept"
-	   ('face 'standoff-arb-konzept));;(:foreground "forest green")))
-	  ("kontext"
-	   ('face 'standoff-arb-kontext))
-	  ))
+    (setq standoff-markup-overlays-after
+	  '(("beispiel"
+	     ('face 'standoff-arb-beispiel));;(:foreground "dark red")))
+	    ("marker"
+	     ('face 'standoff-arb-marker));;(:foreground "dark blue")))
+	    ("konzept"
+	     ('face 'standoff-arb-konzept));;(:foreground "forest green")))
+	    ("kontext"
+	     ('face 'standoff-arb-kontext))
+	    ))
 
-  ;; (setq standoff-relations-allowed
-  ;; 	'(("marker" "markiert" "beispiel")
-  ;; 	  ;; beispiel ... konzept
-  ;; 	  ("beispiel" "istBeispielFür" "konzept")
-  ;; 	  ("beispiel" "erläutert" "konzept")
-  ;; 	  ;; beispiel ... kontext
-  ;; 	  ("beispiel" "erläutert" "kontext")
-  ;; 	  ("beispiel" "stelltVorAugen" "kontext")
-  ;; 	  ("beispiel" "machtHandgreiflich" "kontext")
-  ;; 	  ))
+    ;; (setq standoff-relations-allowed
+    ;; 	'(("marker" "markiert" "beispiel")
+    ;; 	  ;; beispiel ... konzept
+    ;; 	  ("beispiel" "istBeispielFür" "konzept")
+    ;; 	  ("beispiel" "erläutert" "konzept")
+    ;; 	  ;; beispiel ... kontext
+    ;; 	  ("beispiel" "erläutert" "kontext")
+    ;; 	  ("beispiel" "stelltVorAugen" "kontext")
+    ;; 	  ("beispiel" "machtHandgreiflich" "kontext")
+    ;; 	  ))
+    ))
 
-  (load "standoff-arb-owl.el")
-
-)
-(add-hook 'standoff-mode-hook 'standoff-arb-config)
-
-;; (setplist 'beispiel-tag 
-;; 	  '(face (foreground-color . "red")))
-;; (setplist 'marker-tag 
-;; 	  '(face (background-color . "light blue")))
-;; (setplist 'konzept-tag 
-;; 	  '(face (foreground-color . "red")))
-
-
-
+;;; standoff-arb.el ends here.
