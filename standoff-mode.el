b7d1e2896e9f75c6404eff32ec1cbc3b95777350
Christian Lück <christian.lueck@ruhr-uni-bochum.de>
2017-06-02 00:56:05 +0200

    Not using hooks for essential setup any more. No dummy.
    - Instead of setting essential things up by mode-hooks, they are put
      into the mode command. This avoids conflicts when backends need
      these things right away.
    
    - standoff-mode no longer automatically loads the dummy backend.
    
    - Improved commentary section at file header.

--- standoff-mode.el
+++ standoff-mode.el
@@ -33,27 +33,34 @@
 ;; read-only. We then call it the source document, which the external
 ;; markup refers to. There are commands for creating 1) markup
 ;; elements, 2) relations between such elements that take the form of
-;; RDF-like directed graphs; there will soon be commands for creating
-;; 3) attributes of markup elements and 4) writing free text comments
-;; anchored on these elements. Markup elements refer to portions of
-;; the source document by character offsets. Annotations can be dumped
-;; as elisp-expressions to a file. Other persistent layers can be
-;; plugged provided that they implement the API for backends defined
-;; in `standoff-api.el'.
+;; RDF-like directed graphs, 3) attributes of markup elements and 4)
+;; writing free text comments anchored on these elements. Markup
+;; elements refer to portions of the source document by character
+;; offsets.
+
+;; There are several pluggable backends for storing the
+;; annotations. They can be stored in JSON format or as elisp
+;; s-expressions. The API for backends is defined in
+;; `standoff-api.el'.
 
 ;; Usage:
 
-;; Add to your Emacs config:
+;; Add the following lines to your Emacs config:
 
 ;;  (add-to-list 'load-path "/path/to/standoff-mode-directory")
+
 ;;  (autoload 'standoff-mode "standoff-mode.el"
 ;;    "Mode for creating and editing stand-off markup, aka external markup" t)
+
 ;;  ;; auto-load standoff-mode for files ending with .TEI-P5.xml:
 ;;  (add-to-list 'auto-mode-alist '("\\.TEI-P5.xml$" . standoff-mode))
 
+;; You also have to choose a backend for storing the annotations. See
+;; standoff-dummy.el or standoff-json-file.el for instructions.
+
 ;;; Code:
+
 (require 'standoff-api)
-(require 'standoff-dummy)
 (require 'standoff-xml)
 (require 'standoff-relations)
 
@@ -80,7 +87,8 @@ calculated. The hash will show up in the minibuffer."
     (setq-local standoff-source-md5 (md5 (current-buffer))))
   (message "The document's md5 checksum is: %s" standoff-source-md5))
 
-(add-hook 'standoff-mode-hook 'standoff-source-checksum)
+;; Run from mode command directly
+;;(add-hook 'standoff-mode-hook 'standoff-source-checksum)
 
 ;;;; Creating and Deleting Markup
 
@@ -368,8 +376,10 @@ pairs of overlay properties, they are interned to a special
 obarray in order to avoid namespace collisions. We also make this
 special obarray buffer local."
 ;;TODO
-  (setq standoff--overlay-property-obarray nil))
-(add-hook 'standoff-mode-hook 'standoff--overlay-property-obarray-init)
+  (setq-local standoff--overlay-property-obarray nil))
+
+;; Run from mode command directly
+;;(add-hook 'standoff-mode-hook 'standoff--overlay-property-obarray-init)
 
 (defvar standoff--overlay-property-value-format
   "standoff-markup-element-property-symbol-%s-%s")
@@ -429,7 +439,8 @@ instances this functions might need to be rewritten."
   "Make a new hashtable for mapping markup instance ids to numbers."
   (setq-local standoff-markup-number-mapping (make-hash-table :test 'equal)))
 
-(add-hook 'standoff-mode-hook 'standoff-markup-number-mapping-setup)
+;; Run from mode command directly
+;;(add-hook 'standoff-mode-hook 'standoff-markup-number-mapping-setup)
   
 (defun standoff-markup-get-number (buf markup-inst-id)
   "Return the number associated to a markup instance.
@@ -598,7 +609,6 @@ functions for highlightning markup in the current buffer. It
 calls `standoff-highlight-markup-range' to actually highlight a
 markup element or range."
   (let ((markup-elements (funcall standoff-markup-read-function (current-buffer) beg end markup-type markup-inst-id)))
-    ;; (message "from backend: %s" markup-elements)
     ;; build a list from the markup returned by the backend and apply
     ;; it to standoff-highlight-markup-range
     (dolist (range markup-elements)
@@ -623,7 +633,7 @@ markup element or range."
     (standoff-highlight-markup beg end markup-type-or-nil)))
 
 (defun standoff-highlight-markup-buffer (&optional markup-type)
-  "Highlight markup in the backend optionally filtered by markup type."
+  "Highlight markup in the backend optionally filtered by MARKUP-TYPE."
   (interactive
    (list
     (standoff-markup-type-from-user-input
@@ -1027,7 +1037,10 @@ are bound to commands instead.
 
 \\{standoff-mode-map}
 "
-  )
+  ;; run just before running hooks
+  (standoff-markup-number-mapping-setup)
+  (standoff--overlay-property-obarray-init)
+  (standoff-source-checksum))
 
 
 (provide 'standoff-mode)
