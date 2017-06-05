d55ddaf9b5492a2b159ec28d5002ea5224df2df8
Christian Lück <christian.lueck@ruhr-uni-bochum.de>
2017-06-06 01:04:04 +0200

    copyright and license on json file backend

--- standoff-json-file.el
+++ standoff-json-file.el
@@ -1,5 +1,26 @@
 ;;; standoff-json-file.el --- JSON file backend for standoff-mode.
 
+;; Copyright (C) 2017 Christian Lück
+
+;; Author: Christian Lück <christian.lueck@ruhr-uni-bochum.de>
+;; URL: https://github.com/lueck/standoff-mode
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with standoff-mode. If not, see
+;; <http://www.gnu.org/licenses/>.
+
 ;;; Commentary:
 
 ;; Functions for storing to and loading annotations from a JSON file.
