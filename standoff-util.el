0dcb607a1c3a290ae2f0b6a5e99cfb493ee178fd
Christian Lück <christian.lueck@ruhr-uni-bochum.de>
2017-06-06 00:05:21 +0200

    utils: getting and setting source buffer etc.

--- standoff-util.el
+++ standoff-util.el
@@ -4,6 +4,34 @@
 
 ;;; Code:
 
+;;;; getting and setting the source buffer
+
+(defvar standoff-current-source-buffer nil
+  "Current source buffer.")
+
+(defun standoff-util/get-source-buffer ()
+  "Return the current source buffer."
+  (get-buffer standoff-current-source-buffer))
+
+(defun standoff-util/set-source-buffer (buffer)
+  "Set the current source buffer name BUFFER."
+  (if (bufferp buffer)
+      (setq standoff-current-source-buffer (buffer-name buffer))
+    (setq standoff-current-source-buffer buffer)))
+
+;;;; formatting UI output
+
+(defun standoff-util/plural-s (count)
+  "Return a plural s if COUNT does not equal 1."
+  (if (= 1 count) "" "s"))
+
+;;;; UUIDs
+
+(defvar standoff-util/re-uuid
+  "[[:xdigit:]]\\{8\\}-[[:xdigit:]]\\{4\\}-[[:xdigit:]]\\{4\\}-[[:xdigit:]]\\{4\\}-[[:xdigit:]]\\{12\\}"
+  "Regular expression for uuid.
+Matches xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx")
+
 (defun standoff-util/create-uuid (&optional data pos)
   "Create a UUID, using a simple hashing of variable data.
 Example of a UUID: 1df63142-a513-c850-31a3-535fc3520c3dq.  The
