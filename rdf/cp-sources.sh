cbd8052644e7e7b8b79818e491336286209c0240
Christian Lück <christian.lueck@uni-muenster.de>
2021-07-30 20:20:41 +0200

    shell script for copying source files to files named by md5 hashes

---
+++ rdf/cp-sources.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+usage () {
+    echo $1
+    cat <<EOF
+USAGE: $0 DIR FILE_PATTERN
+
+This will copy each file matching FILE_PATTERN to DIR where the file
+name will be the md5 hash value.
+
+Example: $0 sources/*.TEI-P5.xml
+EOF
+
+    exit 1;
+}
+
+if [ $# -lt 2 ]; then usage ""; fi
+if [ ! -d $1 ]; then usage "ERROR: $1: no such directory"; fi
+
+hashdir=$1
+
+shift
+
+for i in "$@"; do cp -nv $i ${hashdir}/$(md5sum $i | awk '{print $1}'); done
